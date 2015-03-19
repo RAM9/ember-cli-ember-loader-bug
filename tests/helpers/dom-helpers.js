02d3c9e3cc7657b47b788736200b7bbeea242fcf
robotarmy <curtis@ram9.cc>
2015-03-19 13:15:56 -0700

    found

--- tests/helpers/dom-helpers.js
+++ tests/helpers/dom-helpers.js
@@ -3,7 +3,7 @@ import Ember from "ember";
 var domHelpers = function() {
 
   Ember.Test.registerHelper('exists', function(app,assert,selector) {
-    return assert.equal(find(selector).length >= 1, true, selector + " not found");
+    return assert.equal(find(selector).length >= 1, true, selector + "  found");
   });
 }();
 
